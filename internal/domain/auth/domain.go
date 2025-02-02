1440c63e26e22c3caa6449922ac84477a6cba4fa
go-jedi <golangjedi@gmail.com>
2025-02-02 16:19:47 +0300

    change sign in dto row last name validate

--- internal/domain/auth/domain.go
+++ internal/domain/auth/domain.go
@@ -15,7 +15,7 @@ type SignInDTO struct {
 	TelegramID string `json:"telegram_id" validate:"required,min=1"`
 	Username   string `json:"username" validate:"required,min=1"`
 	FirstName  string `json:"first_name" validate:"required,min=1"`
-	LastName   string `json:"last_name" validate:"required,min=1"`
+	LastName   string `json:"last_name" validate:"omitempty,min=1"`
 }
 
 // SignInResp represents the response body for a successful sign-in.
