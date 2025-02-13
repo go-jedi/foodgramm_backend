8d6f84128b07fda2875564d1cb651ba210c08c89
go-jedi <golangjedi@gmail.com>
2025-02-13 23:54:30 +0300

    change payment domain file

--- internal/domain/payment/domain.go
+++ internal/domain/payment/domain.go
@@ -2,23 +2,6 @@ package payment
 
 import "math/big"
 
-type GetLinkBody struct {
-	TelegramID *big.Int `json:"telegram_id"`
-	Type       string   `json:"type"`
-}
-
-type GetLinkResponse struct {
-	URL string `json:"url"`
-}
-
-type CheckStatusBody struct {
-	TelegramID *big.Int `json:"telegram_id"`
-}
-
-type CheckStatusResponse struct {
-	Value bool `json:"value"`
-}
-
 //
 // CREATE
 //
@@ -26,4 +9,17 @@ type CheckStatusResponse struct {
 type CreateDTO struct {
 	TelegramID string `json:"telegram_id" validate:"required,min=1"`
 	Type       string `json:"type" validate:"required,oneof=rub stars"`
+}
+
+//
+// GET LINK REQUEST
+//
+
+type GetLinkBody struct {
+	TelegramID *big.Int `json:"telegram_id"`
+	Type       string   `json:"type"`
+}
+
+type GetLinkResponse struct {
+	URL string `json:"url"`
 }
